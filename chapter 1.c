f337420a55cca50c3d915ec5741cc46465d9d3f1
daemonhunter <jman9545@gmail.com>
2026-06-09 22:11:14 -0500

    yuh

--- chapter 1.c
+++ chapter 1.c
@@ -3,7 +3,18 @@
 //
 // Created by daemonhunter on 6/7/26.
 //
+
+void swap(int *pa,int *pb) {
+    int t = *pa;
+    *pa = *pb;
+    *pb = t;
+    printf("a=%d b=%d\n",*pa,*pb);
+}
+
 int main() {
-    puts("Gabby is Gay!");
+    int a = 21;
+    int b = 65;
+    swap(&a,&b);
+
     return EXIT_SUCCESS;
 }
